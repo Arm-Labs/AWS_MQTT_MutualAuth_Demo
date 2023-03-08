f5d1728ee8711d91243ae3e0cd2b0b7f0b459b51
Christopher Seidl <KeilChris@users.noreply.github.com>
2023-03-08 12:12:42 +0100

    Update to reflect the latest changes in the VS Code extensions

--- run_example.sh
+++ run_example.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-VHT_MPS3_Corstone_SSE-300  -f ./fvp_config.txt -a ./out/Demo/Debug/AVH/Demo.Debug+AVH.axf --stat $*
+VHT_MPS3_Corstone_SSE-300  -f ./fvp_config.txt -a ./out/Demo/AVH/Debug/Debug+AVH.axf --stat $*
